374b5fdf257dfc60bfd4939c523b48938a76ed06
Suying1020 <cheahsuying@gmail.com>
2022-06-21 11:33:12 +0800

    Add Identity Tables

--- Todos/Migrations/20220621031204_AddEFCoreIdentity.Designer.cs
+++ Todos/Migrations/20220621031204_AddEFCoreIdentity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Todos.Data;
 
 namespace Todos.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220621031204_AddEFCoreIdentity")]
+    partial class AddEFCoreIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
